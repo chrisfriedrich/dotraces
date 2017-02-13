e6a4b0101ae9b6716f0d4650172bcab694d7fdae
cnf <cnf@uoregon.edu>
2017-02-12 21:57:48 -0800

    Commit of current progress

--- DotRaces/Models/Race.cs
+++ DotRaces/Models/Race.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -32,6 +33,39 @@ namespace DotRaces.Models
         public double Difference14 { get; set; }
         public double Difference15 { get; set; }
         public double Difference16 { get; set; }
+
+        [Display(Name = "Difference 1")]
+        public double? OtherDifference1 { get; set; }
+        [Display(Name = "Difference 2")]
+        public double? OtherDifference2 { get; set; }
+        [Display(Name = "Difference 3")]
+        public double? OtherDifference3 { get; set; }
+        [Display(Name = "Difference 4")]
+        public double? OtherDifference4 { get; set; }
+        [Display(Name = "Difference 5")]
+        public double? OtherDifference5 { get; set; }
+        [Display(Name = "Difference 6")]
+        public double? OtherDifference6 { get; set; }
+        [Display(Name = "Difference 7")]
+        public double? OtherDifference7 { get; set; }
+        [Display(Name = "Difference 8")]
+        public double? OtherDifference8 { get; set; }
+        [Display(Name = "Difference 9")]
+        public double? OtherDifference9 { get; set; }
+        [Display(Name = "Difference 10")]
+        public double? OtherDifference10 { get; set; }
+        [Display(Name = "Difference 11")]
+        public double? OtherDifference11 { get; set; }
+        [Display(Name = "Difference 12")]
+        public double? OtherDifference12 { get; set; }
+        [Display(Name = "Difference 13")]
+        public double? OtherDifference13 { get; set; }
+        [Display(Name = "Difference 14")]
+        public double? OtherDifference14 { get; set; }
+        [Display(Name = "Difference 15")]
+        public double? OtherDifference15 { get; set; }
+        [Display(Name = "Difference 16")]
+        public double? OtherDifference16 { get; set; }
 
         public int? Sound1ID { get; set; }
         public int? Sound2ID { get; set; }
